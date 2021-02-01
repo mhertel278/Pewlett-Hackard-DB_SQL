b838af587cd67ca864ae941c2f19ddc0204302bf
Michael Hertel <mhertel278@gmail.com>
2021-02-01 15:52:34 -0600

    created current_emp table

--- queries.sql
+++ queries.sql
@@ -77,3 +77,39 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
 ;
 
 SELECT * FROM retirement_info;
+--  FROM HERE
+-- Recreate retirement_info table with employee numbers
+DROP TABLE retirement_info;
+
+
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+
+;
+--Check the table
+
+SELECT * FROM retirement_info;
+
+-- Join departments and dept_manager tables
+
+SELECT departments.dept_name,
+	dept_manager.emp_no,
+	dept_manager.from_date,
+	dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+-- Recreate retirement_info including to_date
+
+SELECT retirement_info.emp_no,
+	retirement_info.first_name,
+	retirement_info.last_name,
+	dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+	
