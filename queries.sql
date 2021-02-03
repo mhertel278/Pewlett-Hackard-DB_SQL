5970050407047f03b4ff59d4f06834e361dcf286
Michael Hertel <mhertel278@gmail.com>
2021-02-03 16:15:50 -0600

    added more tables

--- queries.sql
+++ queries.sql
@@ -143,4 +143,90 @@ LEFT JOIN dept_emp as de
 ON ri.emp_no = de.emp_no
 WHERE de.to_date = ('9999-01-01')
 ;
+
+-- added 2.3.21
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO emp_per_dept
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no
+;
+
+SELECT * FROM emp_per_dept;
+
+SELECT * FROM retirement_info;
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+
+-- Join Employees, salaries, and dept_emp 
+--to get table with current emploees filtered to retirement eligible and
+-- emp #, first and last name, gender, salary
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND de.to_date = ('9999-01-01');
+
+
+DROP TABLE emp_info;
+
+SELECT * FROM emp_info;
+
+-- Create table of managers retiring
+
+SELECT dm.dept_no,
+	d.dept_name,
+	dm.emp_no,
+	ce.first_name,
+	ce.last_name,
+	dm.from_date,
+	dm.to_date
+INTO manager_info
+FROM dept_manager as dm
+	INNER JOIN departments AS d
+		ON (dm.dept_no = d.dept_no)
+	INNER JOIN current_emp as ce
+		ON (dm.emp_no = ce.emp_no);	
+
+-- Create table of current employees with department name
+
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info
+FROM current_emp AS ce
+	INNER JOIN dept_emp AS de
+		ON ce.emp_no = de.emp_no
+	INNER JOIN departments AS d
+		ON de.dept_no = d.dept_no;
+	
+-- Create list of current employees in Sales 
+SELECT * 
+INTO dept_info_sales
+
+FROM dept_info
+WHERE dept_name = 'Sales';
+
+-- Create list of current employees in either Sales or Depevelopment
+
+SELECT * 
+INTO dept_info_sales_dev
+
+FROM dept_info
+WHERE dept_name IN('Sales', 'Development');
 	
