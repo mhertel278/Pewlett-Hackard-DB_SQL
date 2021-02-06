4390cae5bb8e051b53d90381730561bc1cfafdd6
Michael Hertel <mhertel278@gmail.com>
2021-02-05 20:52:11 -0600

    created deliverable 1 tables

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,44 +1,49 @@
 -- Get the names, titles and dates for title heald for employees eligible for retirement
 
-SELECT --DISTINCT ON (e.emp_no)
+SELECT 
 	e.emp_no
 	, e.first_name
 	, e.last_name
 	, t.title
 	, t.from_date
 	, t.to_date
--- INTO retirement_titles
+INTO retirement_titles
 FROM employees as e
 JOIN titles as t
 	ON e.emp_no = t.emp_no
+-- JOIN the dept_emp to filter on to_date for current emp
+JOIN dept_emp as de
+	ON e.emp_no = de.emp_no
 -- Filter for retirement eligible ages
 WHERE e.birth_date BETWEEN ('1952-01-01') AND ('1955-12-31') 
  	-- filter to only current employees
-	--AND	t.to_date = ('9999-01-01')	
+	AND	de.to_date = ('9999-01-01')	
 ORDER BY e.emp_no
 	-- show most recent position first
-	, t.to_date DESC
+ 	, t.to_date DESC
+
 ;
 
 
 -- Get only the most recent position for each person in the retirement_titles table
 
-SELECT DISTINCT ON (r.emp_no)
-	r.emp_no
-	,r.first_name
-	,r.last_name
-	,r.title
--- INTO unique_titles
-FROM retirement_titles r
-ORDER BY r.emp_no
-	, r.to_date DESC
+SELECT DISTINCT ON (rt.emp_no)
+	rt.emp_no
+	,rt.first_name
+	,rt.last_name
+	,rt.title
+INTO unique_titles
+FROM retirement_titles AS rt
+ORDER BY rt.emp_no
+	, rt.to_date DESC
 ;
 
 -- Get count of retirement eligible employees by title
-SELECT COUNT(r.emp_no) AS "count"
-	,r.title
---INTO retiring_titles
-FROM retirement_titles r
+SELECT COUNT(ut.emp_no) AS "count"
+	,ut.title
+INTO retiring_titles
+FROM unique_titles AS ut
+GROUP BY ut.title
 ORDER BY count DESC
 ;
 
